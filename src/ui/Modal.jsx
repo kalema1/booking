0b3e0509d2e07031a58d35824b626e46442f141b
kalema1 <kalemar8@gmail.com>
2024-09-30 20:06:33 +0300

    close window on click outside
    - creeated a custom hook called useOutsodeClick.
    - added logic to close window on click outside the window.
    - used the custom hook in the Modal component.

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -2,6 +2,7 @@ import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -77,6 +78,7 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) {
     return null;
@@ -84,7 +86,7 @@ function Window({ children, name }) {
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
