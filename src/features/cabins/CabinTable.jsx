96a9580b2ab2064a8498e44bb7e45081d06b4054
kalema1 <kalemar8@gmail.com>
2024-10-14 09:04:40 +0300

    fix a bug for undefined cabins
    - fixed the bug of the isLoading remaing is True state forever. The bbug
      was caused by the return when isLoading is true not accessible.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,3 @@
-//import styled from "styled-components";
 import { useSearchParams } from "react-router-dom";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
@@ -6,24 +5,17 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 
-/* const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`; */
-
 export default function CabinTable() {
-  const { isLoading, cabins, error } = useCabins();
   const [searchParams] = useSearchParams();
+  const { isLoading, cabins, error } = useCabins();
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return <div>Error Fetching Data</div>;
+  }
 
   // filter cabins
   const filterValue = searchParams.get("discount") || "all";
@@ -49,16 +41,6 @@ export default function CabinTable() {
   const sortedCabins = filteredCabins.sort(
     (a, b) => (a[field] - b[field]) * modifier
   );
-
-  console.log(sortedCabins);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
-  if (error) {
-    return <div>Error Fetching Data</div>;
-  }
 
   return (
     <Menus>
