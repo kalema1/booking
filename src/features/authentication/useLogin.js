0cb0afa3fa7f3d652c021d0b2e1ff03df1d3d4fe
kalema1 <kalemar8@gmail.com>
2024-10-22 21:07:33 +0300

    add the logout feature
    - created th Logout component.
    - added the jsx to the component.
    - created the logout function in the apiAuth.js file.
    - added logic to logout  user.
    - created the useLogout hook. added logic to the hook.
    - used the logic from the hook into the Logout component.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,7 +10,7 @@ export function useLogin() {
 
     onSuccess: (user) => {
       console.log(user);
-      navigate("/");
+      navigate("/", { replace: true });
     },
 
     onError: (err) => {
