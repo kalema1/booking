91b6ac4684a8bebbe7a68c6b1b4a1acabf0b099d
kalema1 <kalemar8@gmail.com>
2024-10-22 21:46:57 +0300

    set query data on login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
   const { mutate: loginUser, isLoading } = useMutation({
     mutationFn: ({ email, password }) => login({ email, password }),
 
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/", { replace: true });
     },
 
